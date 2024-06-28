f4f4e33499d8f648cd2dcdcdc1058279c8e13bba
Granted07 <anjishnu.04032007@gmail.com>
2024-06-28 17:58:06 +0530

    added gui

---
+++ main.py
@@ -0,0 +1,111 @@
+import os,sys
+import whatsapp
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect, QStringListModel,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QAbstractButton, QApplication, QMainWindow, QFileDialog, QDialogButtonBox,
+    QLabel, QPushButton, QSizePolicy, QWidget, QTextBrowser, QTextEdit)
+from PySide6 import QtWidgets
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.setGeometry(400,400,425,332)
+        self.initUI()
+
+    def initUI(self):
+        self.buttonBox = QDialogButtonBox(self)
+        self.buttonBox.setObjectName(u"buttonBox")
+        self.buttonBox.setGeometry(QRect(70, 290, 341, 32))
+        self.buttonBox.setOrientation(Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QDialogButtonBox.Cancel|QDialogButtonBox.Ok)
+        self.buttonBox.rejected.connect(self.reject)
+        self.buttonBox.accepted.connect(self.accept)
+        self.b1 = QPushButton(self)
+        self.b1.setObjectName(u"b1")
+        self.b1.setGeometry(QRect(10, 50, 75, 23))
+        self.label = QLabel(self)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(10, 20, 211, 21))
+        font = QFont()
+        font.setFamilies([u"Montserrat"])
+        font.setPointSize(10)
+        font.setBold(True)
+        self.label.setFont(font)
+        self.b2 = QPushButton(self)
+        self.b2.setObjectName(u"b2")
+        self.b2.setGeometry(QRect(10, 160, 75, 23))
+        self.b1.clicked.connect(self.profileselect)
+        self.b2.clicked.connect(self.openform)
+        self.label_2 = QLabel(self)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(10, 130, 211, 21))
+        self.label_2.setFont(font)
+        self.label_3 = QLabel(self)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(150, 130, 211, 21))
+        self.label_3.setFont(font)
+        self.setWindowTitle(QCoreApplication.translate("Dialog", u"Whatsapp Spammer", None))
+        self.b1.setText(QCoreApplication.translate("Dialog", u"Choose File", None))
+        self.b2.setText(QCoreApplication.translate("Dialog", u"Open", None))
+        self.label.setText(QCoreApplication.translate("Dialog", u"Current selected file:\n ", None))
+        self.label.adjustSize()
+        self.label_2.setText(QCoreApplication.translate("Dialog", u"Edit Form.csv:", None))
+        self.label_2.adjustSize()
+        self.label_3.setText(QCoreApplication.translate("Dialog", u"Enter Message", None))
+        self.label_3.adjustSize()
+        self.textEdit = QTextEdit(self)
+        self.textEdit.setObjectName(u"textEdit")
+        self.textEdit.setGeometry(QRect(150, 160, 251, 121))
+        
+
+    def profileselect(self):
+        global folderName
+        
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        options |= QFileDialog.DontUseCustomDirectoryIcons
+        dialog = QFileDialog()
+        dialog.setOptions(options)
+        x=''
+        for i in os.getcwd().split("\\")[0:3]:
+            x+=i
+            x+='\\\\'
+        dialog.setDirectory(x+'AppData\\Roaming\\Mozilla\\Firefox\\Profiles')
+        folderName = str(dialog.getExistingDirectory(self, "Select Directory"))
+        self.label.setText(QCoreApplication.translate("Dialog", u"Current selected file:\n"+folderName , None))
+        self.label.adjustSize()
+
+    def reject(self):
+        sys.exit(1)
+
+    def openform(self):
+        os.startfile("form.csv")
+
+    def accept(self):
+        csvlist = whatsapp.returnDictData("form.csv","Name", "Number")
+        active_list = csvlist # or test_list
+        msg = self.textEdit.toPlainText()
+        print('Message:',msg)
+        whatsapp.send_message(active_list, msg, False, folderName)
+
+        # filtered = []
+
+        # for user in active_list:
+        #      print(user[2])
+        #      if user[2]==8:
+        #          filtered.append(user)
+
+
+def window():
+    app = QApplication(sys.argv)
+    win = MyWindow()
+    win.show()
+    sys.exit(app.exec())
+
+window()
+
---
+++ whatsapp.py
@@ -0,0 +1,92 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+#from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from time import sleep
+from messages import message_builder
+import os,csv
+
+def link_builder(number:str):
+    if len(number) > 10:
+        number=number[-10:]
+    return f"https://web.whatsapp.com/send?phone=%2B91{number}&text&app_absent=0"
+
+def load_dump():
+    l = []
+    if os.path.exists('./dump.txt'):
+        with open("./dump.txt") as dumpfile:
+            for line in dumpfile.read():
+                l.append(line)
+    else:
+        with open('dump.txt','w') as f:
+            f.close()
+    print(f"Names loaded from already sent: {l}")
+    return l
+
+def type_message(driver:any,message):
+    action_chain = ActionChains(driver)
+    action_chain.send_keys(message) 
+    action_chain.send_keys(Keys.RETURN).perform()
+
+def send_message(links:list,message:str,dry_run:bool,folderName:str):
+    dump = load_dump()
+    print('Dry Run status:',dry_run)
+    options = webdriver.FirefoxOptions()
+    options.add_argument("-profile")
+    options.add_argument(folderName)
+    options.add_argument("-headless") 
+    driver=webdriver.Firefox(options=options)
+    wait = WebDriverWait(driver, 100)
+    count = 0
+    total = len(links)
+    import time
+    for link_tuple in links:
+        start = time.time()
+        count+=1
+        name,link= link_tuple
+        name = name.split()[0]
+        if link in dump:
+            continue
+        print(f"{count} of {total} {link_tuple}")
+        message = message_builder(name, message)
+        try:
+            driver.get(link)
+        except:
+            driver.quit()
+            driver.get(link)
+        driver.execute_script("window.onbeforeunload = function() {};")
+        print('Opening chat')
+        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "html > body > div:nth-of-type(1) > div > div > div:nth-of-type(2) > div:nth-of-type(4) > div > footer > div:nth-of-type(1) > div > span:nth-of-type(2) > div > div:nth-of-type(2) > div:nth-of-type(1) > div > div:nth-of-type(1)")))     
+        if not dry_run:
+            type_message(driver=driver,message=message)
+            sleep(2)
+            with open("./dump.txt","a") as dumpfile:
+                dumpfile.write(f"{link}\n")
+                print(f"Message Sent!\nDumped {name} to file")
+        else:
+            continue
+    end = time.time()
+    print(f"Elapsed time: {end - start}")
+    driver.quit()
+
+def returnDictData(csvfile:str, name_row_name, phone_number_row_name) -> list:
+    with open(csvfile) as induction_form:
+        reader = csv.DictReader(induction_form)
+        phonelist = []
+        for row in reader:
+            # if category.count(','):
+            #     categories_set = set(category.split(", "))
+            # else:
+            #     categories_set = {category}
+
+            phonelist.append((
+                row[name_row_name],
+                link_builder(row[phone_number_row_name]),
+                # int(row[class_row_name]),
+                # categories_set            
+            ))
+
+        return phonelist
